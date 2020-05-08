f89461261428b0190af28d9c70db92a355edef11
Ignacio de la Vega <ignacio.delavega@ing.austral.edu.ar>
2020-05-08 18:44:26 -0300

    Implement connection with postgres

---
+++ server/models/user.ts
@@ -0,0 +1,14 @@
+﻿const {Model, DataTypes} = require('sequelize');
+const {sequelize} = require('../server/server');
+
+class User extends Model {}
+
+User.init({
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    id: { type: DataTypes.STRING, primaryKey: true }
+}, {sequelize, modelName: 'user'});
+
+module.exports = User;
--- server/server/server.ts
+++ server/server/server.ts
@@ -1,4 +1,11 @@
 ﻿﻿import express = require('express');
+const {Sequelize} = require('sequelize');
+export const sequelize = new Sequelize('database', 'user', 'password', {dialect: 'postgres'});
+
+// Models
+const User = require('../models/user.js')
+
+// Controllers
 const startControllers = require('./controllers-initializer.js');
 
 export const app: express.Application = express();
@@ -9,6 +16,15 @@ const startServer = () => {
     });
 };
 
-
 startServer();
 startControllers();
+
+sequelize.sync({force: false})
+    .then(() => User.create({
+    name: "Nacho",
+    id: "123"
+})).then(() => {
+    User.findByPk("123").then((user: User) => {
+        console.log(user);
+    });
+}).then()
