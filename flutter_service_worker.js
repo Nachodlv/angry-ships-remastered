c0fb24c4c63f1ad1f7df441c7a8e33ef258a757d
Ignacio de la Vega <ignacio.delavega@ing.austral.edu.ar>
2020-06-19 16:30:33 -0300

    Update build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,7 +17,7 @@ const RESOURCES = {
 "assets/NOTICES": "3753d2088ef3896b4b044fecc54aded1",
 "index.html": "0f6b6608da159675ab8e65159f2cda31",
 "/": "0f6b6608da159675ab8e65159f2cda31",
-"main.dart.js": "3451dec98ba060b630d513bf4c4f648f"
+"main.dart.js": "6369f1515ccc3b70adbaf521a2ef1f5a"
 };
 
 // The application shell files that are downloaded before a service worker can
